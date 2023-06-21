8d3a9704fd30363d01af4a85fdb97a510e4bae51
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-21 13:31:57 +0300

    Car Details ready

--- client/src/components/Catalog/Details.js
+++ client/src/components/Catalog/Details.js
@@ -4,12 +4,9 @@ import { Link, useParams } from "react-router-dom"
 import { getById } from "../../services/carService"
 
 
-
 const Details = () => {
-
   const {id} = useParams()
-
-  const [car, setCar] = useState(null)
+  const [car, setCar] = useState({})
 
   useEffect(() => {
      getById(id).then(c => setCar(c))
@@ -19,22 +16,18 @@ const Details = () => {
     <section id="listing-details">
     <h1>Details</h1>
     <div className="details-info">
-      <img src="/images/audia3.jpg" alt="car"/>
+      <img src={car.imageUrl} alt="car"/>
       <hr />
       <ul className="listing-props">
-        <li><span>Brand:</span>Audi</li>
-        <li><span>Model:</span>A3</li>
-        <li><span>Year:</span>2018</li>
-        <li><span>Price:</span>25000$</li>
+        <li><span>Brand:</span>{car.brand}</li>
+        <li><span>Model:</span>{car.model}</li>
+        <li><span>Year:</span>{car.year}</li>
+        <li><span>Price:</span>{car.price}$</li>
       </ul>
-      <p className="description-para">
-        Some description of this car. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Sunt voluptate quam nesciunt ipsa veritatis voluptas
-        optio debitis repellat porro sapiente.
-      </p>
+      <p className="description-para">{car.description}</p>
       <div className="listings-buttons">
-        <Link to={`{/edit/${car._id}}`} className="button-list"> Edit </Link>
-        <Link to={`{/delete/${car._id}}`} className="button-list"> Delete </Link>
+        <Link to={`/edit/${car._id}`} className="button-list"> Edit </Link>
+        <Link to={`/delete/${car._id}`} className="button-list"> Delete </Link>
       </div>
     </div>
   </section>
