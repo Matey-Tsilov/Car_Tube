d4acad8fdbb19e65d1d84113ae46e0e67620f0ba
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-23 13:01:30 +0300

    update user and guest nav

--- client/src/components/Common/Header.js
+++ client/src/components/Common/Header.js
@@ -1,23 +1,32 @@
 import { Link } from "react-router-dom"
 
+import { UserContext } from "../../Contexts/UserContext";
+import { useContext } from "react";
+
+
 const Header = () => {
+  const {user} = useContext(UserContext)
+
     return (<header>
         <nav>
           <Link className="active" to="/"> Home </Link>
           <Link to="/cars">All Listings</Link>
           <Link to="/search">By Year</Link>
-          {/* Guest users */}
-          <div id="guest">
+          {
+          user == {}
+
+          ? <div id="guest">
             <Link to="/user/login">Login</Link>
             <Link to="/user/register">Register</Link>
           </div>
-          {/* Logged users */}
-          <div id="profile">
+
+          : <div id="profile">
             <Link>Welcome username</Link>
             <Link to="/my-listings">My Listings</Link>
             <Link to="/cars/create">Create Listing</Link>
             <Link to="/">Logout</Link>
           </div>
+          }
         </nav>
       </header>)
 }
