6960eee2bc7d81baab3d7b9091487daa19707935
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-06-30 16:00:00 +0300

    create search in carService + change some CSS

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -6,7 +6,9 @@ const ep = {
     create: '/data/cars',
     edit: '/data/cars/',
     userCars: (userId) => `/data/cars?where=_ownerId%3D%22${userId}%22&sortBy=_createdOn%20desc`,
+    searchCars: (year) => `/data/cars?where=year%3D${year}`
     // delete: '/data/cars/',
+
 
 }
 
@@ -24,4 +26,7 @@ export async function edit(id, data){
 }
 export async function getMine(id){
     return fetchApi.get(`${ep.userCars(id)}`)
+}
+export async function search(year){
+    return fetchApi.get(`${ep.searchCars(year)}`)
 }
