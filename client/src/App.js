a4814ecfc5d7228dccc99676c7852a1668f3135e
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-07-06 16:37:31 +0300

    implement logic for a react preloader, with additional delay!

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
-//import Header from "./components/Common/Header";
-//import Footer from "./components/Common/Footer";
-//import Home from "./components/Home/Home";
-//import Login from "./components/Login/Login";
-//import Register from "./components/Register/Register";
-// import Catalog from "./components/Catalog/Catalog";
-// import Create from "./components/Create/Create";
-// import Edit from "./components/Edit/Edit";
-// import Search from "./components/Search/Search";
-// import Details from "./components/Catalog/Details";
-// import MyListings from "./components/MyListings/MyListings";
-
 import { UserContext } from "./Contexts/UserContext";
 import { useSessionStorage } from "./Hooks/useSessionStorage";
 import { PrivateGuard } from "./RouteGuards/private";
 import { Preloader } from "./components/Common/Preloader";
 
-const Home = lazy(() => import("./components/Home/Home"));
+
 const Header = lazy(() => import("./components/Common/Header"));
 const Footer = lazy(() => import("./components/Common/Footer"));
+const Home = lazy(() => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(import("./components/Home/Home")), 1000);
+  });
+});
+
 const Login = lazy(() => import("./components/Login/Login"));
 const Register = lazy(() => import("./components/Register/Register"));
 const Catalog = lazy(() => import("./components/Catalog/Catalog"));
-const Create = lazy(() => import("./components/Create/Create"));
+const Create = lazy(() => new Promise(resolve => {
+  setTimeout(() => resolve(import("./components/Create/Create")), 1000)
+}));
 const Edit = lazy(() => import("./components/Edit/Edit"));
 const Search = lazy(() => import("./components/Search/Search"));
 const Details = lazy(() => import("./components/Catalog/Details"));
 const MyListings = lazy(() => import("./components/MyListings/MyListings"));
-
-
 
 function App() {
   const [user, setUser] = useSessionStorage({});
@@ -41,19 +35,21 @@ function App() {
         <Header />
         <main id="site-content">
           <Suspense fallback={<Preloader />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user/login" element={<Login />} />
-            <Route path="/user/register" element={<Register />} />
-            <Route path="/cars" element={<Catalog />} />
-            <Route path="/cars/:id" element={<Details />} />
-            <Route path="/search" element={<Search />} />
-            <Route element={<PrivateGuard />}>
-              <Route path="/cars/create" element={<Create />} />
-              <Route path="/cars/edit/:id" element={<Edit />} />
-              <Route path="/my-listings" element={<MyListings />} />
-            </Route>
-          </Routes>
+
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/user/login" element={<Login />} />
+              <Route path="/user/register" element={<Register />} />
+              <Route path="/cars" element={<Catalog />} />
+              <Route path="/cars/:id" element={<Details />} />
+              <Route path="/search" element={<Search />} />
+              <Route element={<PrivateGuard />}>
+                <Route path="/cars/create" element={<Create />} />
+                <Route path="/cars/edit/:id" element={<Edit />} />
+                <Route path="/my-listings" element={<MyListings />} />
+              </Route>
+            </Routes>
+
           </Suspense>
         </main>
 
