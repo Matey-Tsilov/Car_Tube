fdf283e4baa4df9bc987215e48170b7ea2f120d6
Matey <76016806+Matey-Tsilov@users.noreply.github.com>
2023-07-19 14:56:37 +0300

    typed my first unit test with jest - example!

---
+++ client/src/components/Login/Login.test.js
@@ -0,0 +1,10 @@
+import Login from './Login'
+    import {render, screen } from "@testing-library/react";
+
+test('Should render flawlessly', () => {
+    const text = "Login"
+    
+    render(<Login />);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+});
